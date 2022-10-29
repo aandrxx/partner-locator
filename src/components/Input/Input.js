1bf2696ddf1c16ad95fa987656f71d7fef70c756
aandrxx <59689393+aandrxx@users.noreply.github.com>
2022-10-29 13:58:41 +0300

    Added size and fullWidth props

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -3,15 +3,17 @@ import classNames from 'classnames'
 
 import './input.scss'
 
-const InputComponent = React.forwardRef(({ className="", variant="default", onChange = () => false, ...restProps }, ref) => {
+const InputComponent = React.forwardRef(({ className='', variant='default', fullWidth=false, size='default', onChange = () => false, ...restProps }, ref) => {
 
     return (
         <div className={
                 classNames(
                     {
                         'form__input': true,
+                        '--full-width': fullWidth,
                         '--outlined': variant && variant === 'outlined',
                         '--default': variant && variant === 'default',
+                        '--small': size && size === 'small',
                     },
                     ...className.split(' ')
                 )

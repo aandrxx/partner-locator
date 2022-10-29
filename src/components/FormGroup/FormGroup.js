8354b929a41d0473584324dd756d9af5c4837b9d
aandrxx <59689393+aandrxx@users.noreply.github.com>
2022-10-29 13:58:03 +0300

    Changed className

--- src/components/FormGroup/FormGroup.js
+++ src/components/FormGroup/FormGroup.js
@@ -17,7 +17,7 @@ const FormGroupComponent = ({
         <div className={
                 classNames(
                     {
-                        'form__input_group': true,
+                        'form__item_group': true,
                         '--full_width': fullWidth,
                         '--outlined': variant && variant === 'outlined',
                         '--small': size && size === 'small',
